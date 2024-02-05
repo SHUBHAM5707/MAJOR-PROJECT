f17f4aa246bafa40e1568b095e7e4d4e60cc2302
SHUBHAM5707 <loharshubham96@gmail.com>
2024-02-05 11:59:26 +0530

    Firebase setup completed (video no 32-33)

--- shop_kart/lib/main.dart
+++ shop_kart/lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shop_kart/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 ///---entry of flutter apk----
-void main(){
+Future<void> main() async {
+
+
+
+  //Todo:Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
 
   runApp(const App());
 }
